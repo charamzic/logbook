715d76646629594cdec30cd2f99b7ba8f54f84dd
honza <charamza@gmail.com>
2024-08-25 22:14:22 +0200

    add last mohycane fight to make it run in prod, that's it for today

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,6 @@
 import "@hotwired/turbo-rails";
 import "./controllers";
 
-import * as bootstrap from "bootstrap";
 import { initializeAutocomplete } from "./autocomplete.js";
 
 document.addEventListener("turbo:load", function () {
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,11 +1,8 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
+pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "autocomplete", to: "autocomplete.js"
-pin "bootstrap" # @5.3.3
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript"
